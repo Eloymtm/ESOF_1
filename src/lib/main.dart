a9fe5de6ff1e9cb8d897aaf18cf38fdcbacc56c3
Eduardo Santos <edu.santos04.vc@gmail.com>
2024-04-10 22:39:13 +0100

    Login page almost done

--- src/lib/main.dart
+++ src/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:src/pages/map_page.dart';
 
 void main() {
@@ -12,13 +13,82 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
 
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: MapPage(),
+      home: HomePage(),
+    );
+  }
+}
+
+
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(height: 80),
+            Padding(
+              padding: EdgeInsets.all(90),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Text("UniLift", style: TextStyle(color: const Color.fromRGBO(246, 161, 86, 1), fontSize: 50,fontWeight: FontWeight.w700)),
+                  SizedBox(height: 30),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(30),
+              child: Column(
+                children: <Widget>[
+                  TextField(
+                    decoration: InputDecoration(
+                      hintText: "Enter your student email...",
+                      hintStyle: TextStyle(color: Colors.grey),
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: "Enter your password",
+                      hintStyle: TextStyle(color: Colors.grey),
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: Text("Forgot Password?", style: TextStyle(color: Colors.grey)),
+                  ),
+                  SizedBox(height: 40),
+                  MaterialButton(
+                    onPressed: () {},
+                    height: 50,
+                    minWidth: 200,
+                    color: const Color.fromRGBO(246, 161, 86, 1),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                    child: Text("Login", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 20)),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
