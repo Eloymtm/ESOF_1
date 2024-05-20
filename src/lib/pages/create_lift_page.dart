69deb916c38a07ec26104813026f8178f399f93d
Eloymtm <eloy.mtm@gmail.com>
2024-05-20 04:19:34 +0100

    Algumas melhorias(criar viagem já funciona bem)

--- src/lib/pages/create_lift_page.dart
+++ src/lib/pages/create_lift_page.dart
@@ -6,6 +6,7 @@ import 'package:flutter/widgets.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:src/components/autocomplete.dart';
 import 'package:src/helper/globalVariables.dart';
+import 'package:intl/intl.dart';
 import 'package:src/pages/profile/appbar_widget.dart';
 import 'package:src/pages/profile/button_widget.dart';
 import 'package:src/pages/profile/edit_field_widget.dart';
@@ -16,19 +17,34 @@ class CreateLiftPage extends StatefulWidget {
   @override
   State<CreateLiftPage> createState() => _CreateLiftPageState();
 }
+  Timestamp createTimestamp(String Date, String Time){
+    final dateTimeString = '$Date $Time';
+    final DateFormat dateTimeFormat = DateFormat('yyyy-MM-dd HH:mm');
+    DateTime dateTime = dateTimeFormat.parse(dateTimeString);
 
+    // Adjust for UTC+1 (if necessary)
+    dateTime = dateTime.add(Duration(hours: 1));
 
- Future createRide(String destino, String partida,String DataPartida) async{
+    // Return the Timestamp from Firestore
+    return Timestamp.fromDate(dateTime);
+  }
+
+ Future createRide(String Car, String destino, String partida,String Date,String Time) async{
    final currentUser = FirebaseAuth.instance.currentUser!;
+   final DataPartida = createTimestamp(Date, Time);
+    List<DocumentReference> passageiros = [];
 
    final rideId = FirebaseFirestore.instance.collection('Ride').doc().id; //gerar id
-    final userRef = FirebaseFirestore.instance.collection('User').doc(currentUser.uid); //ir buscar o user
+   final userRef = FirebaseFirestore.instance.collection('User').doc(currentUser.uid); //ir buscar o user
+   passageiros.add(userRef);
     await FirebaseFirestore.instance.collection('Ride').doc(rideId).set(
         {
           'Driver' : userRef,
           'Destino' :destino,
           'Partida' : partida,
-          
+          'HoraPartida' :DataPartida,
+          'Car' : Car,
+          'passageiros': passageiros,
         }
     );
  }
@@ -37,9 +53,10 @@ class _CreateLiftPageState extends State<CreateLiftPage> {
   final LocalPartida = TextEditingController();
   final LocalDestino = TextEditingController();
   final DataPartida = TextEditingController();
+  final Car = TextEditingController();
 
-  TextEditingController _dateController = TextEditingController();
-  TextEditingController _timeController = TextEditingController();
+  TextEditingController TimeController = TextEditingController();
+  TextEditingController DateController = TextEditingController();
 
   double _borderRadius = 15.0;
 
@@ -86,7 +103,7 @@ class _CreateLiftPageState extends State<CreateLiftPage> {
                 const SizedBox(height: 30,),
                 const Text("Data de partida"),
                 TextField(
-                  controller: _dateController,
+                  controller: DateController,
                   decoration: InputDecoration(
                     labelText: 'DATA',
                     filled: true,
@@ -108,7 +125,7 @@ class _CreateLiftPageState extends State<CreateLiftPage> {
                 const SizedBox(height: 10),
                 const Text("Hora de partida"),
                 TextField(
-                  controller: _timeController,
+                  controller: TimeController,
                   decoration: InputDecoration(
                     labelText: 'HORA',
                     filled: true,
@@ -130,7 +147,7 @@ class _CreateLiftPageState extends State<CreateLiftPage> {
                 const SizedBox(height: 30,),
                 const Text("Carro"),
                 TextFormField(
-                  controller: DataPartida,
+                  controller: Car,
                   decoration: InputDecoration(
                     labelText: 'Seleciona o teu carro',
                     suffixIcon: Icon(FontAwesomeIcons.car),
@@ -142,7 +159,7 @@ class _CreateLiftPageState extends State<CreateLiftPage> {
                 const SizedBox(height: 30,),
                 ElevatedButton(
                   onPressed: () {
-                    createRide(LocalDestino.text, LocalPartida.text, DataPartida.text);
+                    createRide(Car.text, LocalDestino.text, LocalPartida.text, DateController.text, TimeController.text);
                     // Mostra uma mensagem de sucesso
                     ScaffoldMessenger.of(context).showSnackBar(
                       SnackBar(content: Text('Viagem criada com sucesso')),
@@ -170,7 +187,7 @@ class _CreateLiftPageState extends State<CreateLiftPage> {
 
     if(_picked != null){
       setState(() {
-        _dateController.text = _picked.toString().split(" ")[0];
+        DateController.text = _picked.toString().split(" ")[0];
       });
     }
   }
@@ -183,7 +200,7 @@ class _CreateLiftPageState extends State<CreateLiftPage> {
 
     if (pickedTime != null) {
       setState(() {
-        _timeController.text = pickedTime.format(context);
+        TimeController.text = pickedTime.format(context);
       });
     }
   }
