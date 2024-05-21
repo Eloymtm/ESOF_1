fc5fcfca2e12c1eefe803a29a35590beb41aee0a
AnaSofiaBaptista <anasofiab.27@gmail.com>
2024-05-21 01:03:11 +0100

    Some tests done

--- src/test/pages/register_test.dart
+++ src/test/pages/register_test.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:src/pages/login_page.dart';
 import 'package:src/pages/register_page.dart';
 
 
@@ -14,12 +15,29 @@ void main() {
     });
 
     testWidgets('Check if all text fields are present', (WidgetTester tester) async {
-      await tester.pumpWidget(testableWidget(widget));
+      await tester.pumpWidget(MaterialApp(home: RegisterPage()));
 
       expect(find.byKey(const Key("emailField")), findsOneWidget);
       expect(find.byKey(const Key("nameField")), findsOneWidget);
       expect(find.byKey(const Key("passwordField")), findsOneWidget);
       expect(find.byKey(const Key("confirmPasswordField")), findsOneWidget);
+    });
+
+    testWidgets('Already have an account button navigates to Login Page', (WidgetTester tester) async {
+      await tester.pumpWidget(
+          MaterialApp(
+          home: RegisterPage(),
+          routes: {
+            '/login_page': (context) => LoginPage(),
+          },
+        ),
+      );
+
+      await tester.tap(find.text('Já tem conta?'));
+      await tester.pumpAndSettle();
+      
+
+      expect(find.text('UNILIFT'), findsOneWidget);
     });
   });
 }
